e5755e83cac12ae319d243382758e46b4f80cd72
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-12-10 16:13:15 +0700

    Initial commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Design-Pro</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div class="root">
+    <header>
+        <div class="logo-wrapper">
+            <img class="logo" src="assets/images/logo.png" alt="Логотип"/>
+        </div>
+        <div class="title-wrapper">
+            <span class="title">Design-Pro Inc.</span>
+        </div>
+    </header>
+    <div class="main">
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Design-Pro</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div class="root">
+    <header>
+        <div class="logo-wrapper">
+            <img class="logo" src="assets/images/logo.png" alt="Логотип"/>
+        </div>
+        <div class="title-wrapper">
+            <span class="title">Design-Pro Inc.</span>
+        </div>
+    </header>
+    <div class="main">
+        <div class="navbar">
+            <a href="">Главная</a>
+            <a href="">Личный кабинет</a>
+            <a href="">Регистрация</a>
+            <a href="?r=site/login">Авторизация</a>
+        </div>
+        <div class="content">
+            Hggfhfj
+        </div>
+    </div>
+    <footer>
+        <div>
+            Copyright &copy; Design-Pro <?= date('Y') ?>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
